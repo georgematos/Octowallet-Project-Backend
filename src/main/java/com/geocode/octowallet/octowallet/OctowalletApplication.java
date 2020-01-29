540cc58d3d2b8c7cedbe9f46e8dd00001df91a17
George Matos <georgemattos@gmail.com>
2020-01-29 14:35:45 -0300

    start springboot project: H2 pre-configuration

---
+++ src/main/java/com/geocode/octowallet/octowallet/OctowalletApplication.java
@@ -0,0 +1,13 @@
+package com.geocode.octowallet.octowallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctowalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctowalletApplication.class, args);
+	}
+
+}
