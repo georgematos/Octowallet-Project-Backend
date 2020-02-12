bc9e577fe2ccc73eabd0fa8188b9e9d3109be48b
George Matos <georgemattos@gmail.com>
2020-02-12 00:06:22 -0300

    [test] implementaçao de testes do CasualExpenseResource

--- src/test/java/com/geocode/octowallet/octowallet/CasualExpenseResourceTests.java
+++ src/test/java/com/geocode/octowallet/octowallet/CasualExpenseResourceTests.java
@@ -1,14 +1,24 @@
 package com.geocode.octowallet.octowallet;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.util.Arrays;
+
 import com.geocode.octowallet.resources.CasualExpenseResource;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+// import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * CasualExpenseResourceTests
@@ -23,18 +33,40 @@ public class CasualExpenseResourceTests extends DemoApplicationTests {
   @BeforeEach
   public void setup() {
     System.out.println("BeforeAll called");
-    mockMvc = MockMvcBuilders.standaloneSetup(casualExpenseResource).build();
+    mockMvc = standaloneSetup(casualExpenseResource).build();
   }
 
   @Test
-  void findAllTest() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/casualexpenses")).andExpect(MockMvcResultMatchers.status().isOk());
+  void getAllCasualExpenseTest() throws Exception {
+
+    mockMvc.perform(get("/casualexpenses")).andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$", hasSize(2)))
+        .andExpect(jsonPath("$[0].id", is(1)))
+        .andExpect(jsonPath("$[0].description", is("Fonte 600w")))
+        .andExpect(jsonPath("$[0].value", is(400.0)))
+        .andExpect(jsonPath("$[0].date", is(Arrays.asList(2020, 2, 1))))
+        .andExpect(jsonPath("$[1].id", is(2)))
+        .andExpect(jsonPath("$[1].description", is("God of War")))
+        .andExpect(jsonPath("$[1].value", is(69.0)))
+        .andExpect(jsonPath("$[1].date", is(Arrays.asList(2019, 12, 13))));
   }
 
   @Test
-  public void testPost() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/casualexpenses"))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("descricao", "value", "date"))
-        .andExpect(MockMvcResultMatchers.status().isCreated());
+  void getCasualExpenseByIdTest() throws Exception {
+
+    mockMvc.perform(get("/casualexpenses/1")).andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.id", is(1)))
+        .andExpect(jsonPath("$.description", is("Fonte 600w")))
+        .andExpect(jsonPath("$.value", is(400.0)))
+        .andExpect(jsonPath("$.date", is(Arrays.asList(2020, 2, 1))));
   }
+
+  @Disabled
+  @Test
+  void postCasualExpenseTest() throws Exception {
+    //TODO Implementar teste de post
+  }
+
 }
